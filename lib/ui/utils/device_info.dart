4633438b49e32918b4731163e0dadc2f3856cbc3
Diego <diego-boos@hotmail.com>
2023-11-02 00:00:27 -0400

    Corrección device info

--- lib/ui/utils/device_info.dart
+++ lib/ui/utils/device_info.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:device_information/device_information.dart';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -6,18 +7,30 @@ import '../../domain/entities/equipo_entity.dart';
 class DeviceInfo {
   static Future<EquipoEntity?> infoDispositivo() async {
     EquipoEntity datosEquipo = EquipoEntity();
-    var status = await Permission.phone.status;
-    if (!status.isGranted) {
-      status = await Permission.phone.request();
-    } else {
-      final imei = await DeviceInformation.deviceIMEINumber;
-      final deviceModel = await DeviceInformation.deviceModel;
 
-      datosEquipo.imei = imei;
-      datosEquipo.modeloEquipo = deviceModel;
+    if (Platform.isAndroid) {
+      var status = await Permission.phone.status;
+      if (!status.isGranted) {
+        status = await Permission.phone.request();
+      }
 
-      return datosEquipo;
+      if (status.isGranted) {
+        return await retrieveDeviceInfo(datosEquipo);
+      }
+    } else if (Platform.isIOS) {
+      return await retrieveDeviceInfo(datosEquipo);
     }
     return null;
+  }
+
+  static Future<EquipoEntity> retrieveDeviceInfo(
+      EquipoEntity datosEquipo) async {
+    final imei = await DeviceInformation.deviceIMEINumber;
+    final deviceModel = await DeviceInformation.deviceModel;
+
+    datosEquipo.imei = imei;
+    datosEquipo.modeloEquipo = deviceModel;
+
+    return datosEquipo;
   }
 }
